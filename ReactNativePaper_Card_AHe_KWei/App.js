fd8f83ec4c0c64816b7408281e8c6bfbf6adfd65
esther0213 <138899462+Estherkim0213@users.noreply.github.com>
2024-02-09 12:41:11 -0600

    trying to add car with divider

--- ReactNativePaper_Card_AHe_KWei/App.js
+++ ReactNativePaper_Card_AHe_KWei/App.js
@@ -1,21 +1,52 @@
-import * as React from 'react';
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import * as React from "react";
+import { Avatar, Button, Card, Text } from "react-native-paper";
+import { View, Image } from "react-native";
 
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
-const MyComponent = () => (
-  <Card>
-    <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
-    <Card.Content>
-      <Text variant="titleLarge">Card title</Text>
-      <Text variant="bodyMedium">Card content</Text>
-    </Card.Content>
-    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>
-);
+const users = [
+  {
+     name: 'brynn',
+     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+  },
+]
 
-export default MyComponent;
+export default function MyComponent() {
+
+  return (
+    <View>
+      <Card>
+        <Card.Title
+          title="Card Title"
+          subtitle="Card Subtitle"
+          left={LeftContent}
+        />
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+        <Card.Cover source={require('./app/assets/travel.jpg')}/>
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
+
+      <Card>
+        <Card.Title>CARD WITH DIVIDER</Card.Title>
+        <Card.Divider />
+        {users.map((u, i) => {
+            <View key={i} /*style={styles.user}*/>
+              <Image
+              style={{ width: 50, height: 50 }} // You can adjust the width and height as per your requirement
+              resizeMode="cover"
+              source={{ uri: u.avatar }}
+              />
+              <Text /*style={styles.name}*/>{u.name}</Text>
+            </View>
+          
+        })}
+      </Card>
+    </View>
+  );
+}
